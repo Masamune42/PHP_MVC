6a291590a044484f32962e3d716e84e7a25da129
alex <a.caignard@hotmail.fr>
2020-03-07 15:04:45 +0100

    création de config.php et db.php dans _config

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 
 // Définition de la page courante
-if(isset($_GET['page']) && !empty($_GET['page']))
-{
+if (isset($_GET['page']) && !empty($_GET['page'])) {
     $page = trim(strtolower($_GET['page']));
 } else {
     $page = 'home';
@@ -12,12 +11,11 @@ if(isset($_GET['page']) && !empty($_GET['page']))
 $allPages = scandir('controllers/');
 
 // Si dans la liste des pages il y a celle que l'on veut
-if(in_array($page.'_controller.php', $allPages))
-{
+if (in_array($page . '_controller.php', $allPages)) {
     // Inclusion de la page
-    include_once 'models/'.$page.'_model.php';
-    include_once 'controllers/'.$page.'_controller.php';
-    include_once 'views/'.$page.'_view.php';
+    include_once 'models/' . $page . '_model.php';
+    include_once 'controllers/' . $page . '_controller.php';
+    include_once 'views/' . $page . '_view.php';
 } else {
     // Retour d'une erreur
     echo 'Erreur 404';
